451c636dd702de947ea544a973d5047a8794223c
mshimada <mshimada2@mail.csuchico.edu>
2017-12-07 14:58:40 -0800

    12/7/2017

--- main.cpp
+++ main.cpp
@@ -1,123 +1,177 @@
 //Author: McDermott Liam Shimada 
-
 #include<array>
 #include<string>
 #include<iostream>
 #include<fstream>
 #include<ostream>
 //#include"numbers.h"
-
-
 using namespace std;
 
 //prototypes
-
-void solveAttempts(); 
+//string winner(variable here);
+void checkAttempts(int); 
+void display_message(void);
 
 
 int main()
-
 {
+        int attempts = 0;
+        //numbers game;
+        string start;
+        string answer;
+        int questions[] = {45, 11, 105000};
 
-numbers game;
-string start;
+        //start game
+        cout << "Ready to play a game? Type in Play or play to start! \n When you are done control c to quit" << endl;
+        cin >> start;
 
-
-
-cout<<"                 "<<endl;
-cout<<"Welcome to Countdown! The rules are simple. "<<endl; 
-cout<<"You will first be shown a number. "<<endl; 
-cout<<"Next, 6 different numbers will be shown "<<endl;
-cout<<"that will need to be solved using mathematical "<<endl;
-cout<<"means to equal the number that was first shown to you. "<<endl;
-cout<<"Depending on the level of difficulty that you select, "<<endl;
-cout<<"you will be giving a certain amount of time to solve. "<<endl;
-cout<<"If you run out of time, you lose! Now lets play! "<<endl;
-
-do
-
-{
-
-cout<<"Please enter Play to begin: "<<endl;
-cin>>start;
-
-if(start == "play" || start == "Play")
-
-{
-
-cout<<"Lets play! "<<endl;
+        while(start == "play" || "Play")
+        {
+                cout << "Lets Play" << endl;
 
 
+                //       while(attempts > 3){
 
-}
+                //              display_message();
+
+                //do while to check to see if the user wants to play
+
+                /* do{
+
+                   cout<<"Please enter Play to begin or control c to quit: "<<endl;
+                   cin>>start;
+
+                   if(start == "play" || start == "Play"){
+
+                   cout<<"Lets play! "<<endl;
+
+                   }
+
+                   }while(start != "play" && start != "Play");
+
+                 */
+                //begin solving for the answer
+
+                for(int i = 0; i < 2; i++){
+
+                        cout<<questions[22]<<endl;
+                        cout<<"Use these numbers to mathematically solve for the upcoming numbers. "<<endl;
+                        cout<<questions[i]<<endl;
+                        cin>>answer;
+
+                        switch(i){
+
+                                case 1: 
+
+                                        if(answer == "((36*100/60)-(50-35)"){
+
+                                                // winner();
 
 
-
-}while(start != "play" && start != "Play");
- 
-
-while(start == "play" || start == "Play")
-
-
-
-
-
-return 0;
-
-}
-
-
-
-
-
-
-void solveAttempts()
-
-
-{
-
-int attempts;
+                                        }
 
 
 
-while(attempts <= 3 )
+                                        else{
 
-{
+                                                attempts += 1;
+                                                checkAttempts(attempts);
 
-if (attempts == 1)
+                                        }
 
-{
+                                        break;
 
-    cout<<"Wrong! You have two more attempts left to solve. "<<endl;
-
-}
-
-else if (attempts == 2)
-
-{
-
-    cout<<"Wrong! You have one more attempt left to solve. "<<endl;
-
-}
-
-else 
-
-{
+                                case 2:
 
 
-    cout<<"Wrong! You lose! "<<endl;
+                                        if(answer == "(36-35)-(50-35)"){
+
+                                                //winner();
+
+                                        }
+
+
+                                        else{
+
+                                                attempts += 1;
+                                                checkAttempts(attempts);
+
+                                        }
+
+                                        break;
+
+                                case 3: 
+
+                                        if(answer == "((36*100/60)-(50-35)"){
+
+                                                //winner();
+
+                                        }
+
+                                        else{
+
+                                                attempts += 1;
+                                                checkAttempts(attempts);
+
+                                        }
+
+                                        break;
+
+                        }
+                }
+
+
+        }
+        return 0;
+
+        }
+
+        void checkAttempts(int attempts)
+
+        {
+
+                if(attempts == 1){
+
+                        cout<<"Wrong! You have 2 more attempts. "<<endl;
+                }
+
+
+                if(attempts == 2){
+
+                        cout<<"Wrong! You have 1 more attempts. "<<endl;
+
+                }
+
+                else if(attempts == 3){
+
+                        cout<<"Wrong! You lose. "<<endl;
+                        attempts = 0;
+                }
+
+                else{
+
+                        cout<<"Error: Restart the program. "<<endl;
+
+
+                }
+        }
+
+        void display_message()
+
+        {
+
+
+                cout<<"I display a message "<<endl;
+
+        }
+
+
+        //end program
 
 
 
-}
-
-}
 
 
-}
-
-
-//correctAnswer
 
 
 
